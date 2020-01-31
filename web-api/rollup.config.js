753a34e25e763e4f4e4e3078c6ca8a6df539ba0f
schiughi <schiughi@gmail.com>
2020-01-31 13:56:16 +0900

    chore: fix lint error and add rollup.config.js to .npmignore

--- web-api/rollup.config.js
+++ web-api/rollup.config.js
@@ -2,26 +2,26 @@ import typescript from "rollup-plugin-typescript2";
 import { terser } from "rollup-plugin-terser";
 
 export default {
-  input: './src/index.ts',
+  input: "./src/index.ts",
   output: [
     {
       file: "./dist/index.js",
-      format: "cjs",
+      format: "cjs"
     },
     {
       file: "./dist/index.mjs",
-      format: "esm",
-    },
+      format: "esm"
+    }
   ],
   plugins: [
     typescript({
       tsconfigOverride: {
         compilerOptions: {
           module: "es2015",
-          moduleResolution: "node",
-        },
-      },
+          moduleResolution: "node"
+        }
+      }
     }),
     terser()
-  ],
+  ]
 };
