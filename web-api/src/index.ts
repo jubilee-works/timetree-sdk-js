20bbab8c53dfff158fe06a45938c5a62c65cd83c
hilott <hilott.g@gmail.com>
2020-09-04 22:02:33 +0900

    chore: add JwtAuthenticator, JwtTimeTreeClient to index.ts

--- web-api/src/index.ts
+++ web-api/src/index.ts
@@ -2,3 +2,5 @@ export * from "./types";
 
 export { TimeTreeClient } from "./TimeTreeClient";
 export { Authenticator } from "./Authenticator";
+export { JwtTimeTreeClient } from "./JwtTimeTreeClient";
+export { JwtAuthenticator } from "./JwtAuthenticator";
